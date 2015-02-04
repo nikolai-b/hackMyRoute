b339703a4425ed7bed87a22ded6f5f0e3f2281dd
Nikolai Berkoff <nikolai.berkoff@blacksquaremedia.com>
2015-02-04 09:52:02 +0000

    fix, label long and lat correctly

--- app/coordinate.rb
+++ app/coordinate.rb
@@ -1,6 +1,6 @@
-Coordinate = Struct.new(:lat, :long) do
+Coordinate = Struct.new(:long, :lat) do
   def ==(other)
-    self.lat.round(5) == other.lat.round(5) && self.long.round(5) == other.long.round(5)
+    self.long.round(5) == other.long.round(5) && self.lat.round(5) == other.lat.round(5)
   end
 end
 
