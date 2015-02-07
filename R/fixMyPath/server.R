4fe1b23f3c591ffc8fee7f1f93b4fc5b024147af
Robinlovelace <rob00x@gmail.com>
2015-02-07 14:27:02 +0000

    Big updates

--- R/fixMyPath/server.R
+++ R/fixMyPath/server.R
@@ -2,14 +2,14 @@ library(shiny)
 library(leaflet)
 library(ggmap)
 library(rgdal)
-library(foreign)
+library(dplyr)
 
 # setwd("R/fixMyPath") # go into the directory if running in rstudio
 
 # Load data
 map_centre <- geocode("Leeds")
 l <- readRDS("al.Rds")
-# source("colorsave.R")
+
 l$color <- "green"
 l$color[grepl("fast", rownames(l@data))] <- "red"
 
@@ -17,10 +17,18 @@ leeds <- readRDS("leeds-msoas-simple.Rds") %>%
   spTransform(CRS("+init=epsg:4326"))
   feature = "cycleparking"
 
+  # Add census data to leeds
+  ldata <- read.csv("leeds-msoa-data.csv")
+  ldata <- rename(ldata, geo_code = CODE)
+  ldata <- inner_join(leeds@data, ldata)
+  leeds@data <- ldata
+  leeds$color_pcycle <- cut(leeds$pCycle, breaks = quantile(leeds$pCycle), labels = brewer.pal(4, "PiYG") )
+
+
 shinyServer(function(input, output){
 
 cents <- coordinates(leeds)
-cents <- SpatialPointsDataFrame(cents, data = leeds@data)
+cents <- SpatialPointsDataFrame(cents, data = leeds@data, match.ID = F)
 
   #     addPopups(-1.549, 53.8, 'First ever popup in leaflet') # add popup
 
@@ -30,7 +38,9 @@ cents <- SpatialPointsDataFrame(cents, data = leeds@data)
       addTiles() %>%
       setView(lng = map_centre[1], lat = map_centre[2], zoom = 10) %>%
       addPolygons(data = leeds
+        , fillOpacity = 0.4
         , opacity = input$transp_zones
+        , fillColor = leeds$color_pcycle
       ) %>%
       addPolylines(color = l$color, opacity = input$transp_fast) %>%
       addCircles(data = cents, color = "black") %>%
